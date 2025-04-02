0738929da838882f063ec4a4b54b52e592ef14d0
nitesh <niteshchaurasiya193@gmail.com>
2025-04-03 02:17:48 +0530

    added avg files and some style

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { WorkerHttpvfs } from "sql.js-httpvfs";
+import Logo from "./assets/books-and-people-svgrepo-com.svg";
+import SearchIcon from "./assets/search_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
 
 const Home = (db: { db: WorkerHttpvfs }) => {
   const [data, setData] = useState<unknown[]>([]);
@@ -12,21 +14,33 @@ const Home = (db: { db: WorkerHttpvfs }) => {
     fetchData();
   }, []);
   return (
-    <div className="books-grid">
-      {data.length ? (
-        data.map((v: any) => {
-          return (
-            <div key={v.id} className="card">
-              <img alt={v.title} src={v.image_url} />
-              <h3>{v.title}</h3>
-              <h5>{v.authors}</h5>
-            </div>
-          );
-        })
-      ) : (
-        <>Home</>
-      )}
-    </div>
+    <>
+      <div className="banner">
+        <Logo />
+        <h2 className="page_name">Book-shelf</h2>
+        <div className="page_actions">
+          <span>
+            <SearchIcon />
+          </span>
+          <h2>About</h2>
+        </div>
+      </div>
+      <div className="books-grid">
+        {data.length ? (
+          data.map((v: any) => {
+            return (
+              <div key={v.id} className="card">
+                <img alt={v.title} src={v.image_url} />
+                <h3>{v.title}</h3>
+                <h5>{v.authors}</h5>
+              </div>
+            );
+          })
+        ) : (
+          <>Home</>
+        )}
+      </div>
+    </>
   );
 };
 export default Home;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/book-shelf/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+  ],
 });
